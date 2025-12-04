ae0579015954029e700307e25abc8c3e66cdaece
Андрій <gapeev1979@gmail.com>
2025-12-04 22:27:19 +0200

    correct on Formik'

--- components/Filters/AllFilters.tsx
+++ components/Filters/AllFilters.tsx
@@ -8,6 +8,24 @@ import FilterLocation from "./FilterLocation";
 import CamperList from "../CamperList/CamperList";
 import FilterType from "./FilterType";
 import FilterEquipment from "./FilterEquipment";
+import { Formik, Form, Field, FormikHelpers } from "formik";
+import { useId } from "react";
+
+interface OrderFormValues {
+  username: string;
+  email: string;
+  deliveryTime: string; //location - Селект
+  restrictions: string[]; // equipment - Чекбокси
+  delivery: string; //  type  -  Радіокнопки
+}
+
+const initialValues: OrderFormValues = {
+  username: "",
+  email: "",
+  deliveryTime: "",
+  restrictions: [],
+  delivery: "pickup",
+};
 
 const AllFilters = () => {
   const [searchLocation, setSearchLocation] = useState("");
@@ -40,20 +58,90 @@ const AllFilters = () => {
     placeholderData: keepPreviousData,
   });
 
+  const fieldId = useId();
+
+  const handleSubmit = (
+    values: OrderFormValues,
+    actions: FormikHelpers<OrderFormValues>
+  ) => {
+    console.log("Order data:", values);
+    actions.resetForm();
+  };
   return (
-    <section>
-      <FilterLocation
-        valueLocation={searchLocation}
-        onSearch={debouncedSearch}
-      />
-      <FilterEquipment
-        valueEquipment={equipmentFilters}
-        onSearch={setEquipmentFilters}
-      />
-      <FilterType value={valueType} onSearch={setValueType} />
-      <CamperList campers={data?.campers || []} />
-    </section>
+    <>
+      <section>
+        <FilterLocation
+          valueLocation={searchLocation}
+          onSearch={debouncedSearch}
+        />
+        <FilterEquipment
+          valueEquipment={equipmentFilters}
+          onSearch={setEquipmentFilters}
+        />
+        <FilterType value={valueType} onSearch={setValueType} />
+        <CamperList campers={data?.campers || []} />
+        <div>
+          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+            <Form>
+              <Field type="text" name="username" />
+              <Field type="email" name="email" />
+              {/* -----  location   ------ */}
+              <div>
+                <label htmlFor={`${fieldId}-deliveryTime`}>
+                  Preferred delivery time
+                </label>
+                <Field
+                  as="select"
+                  name="deliveryTime"
+                  id={`${fieldId}-deliveryTime`}
+                >
+                  <option value="">-- Choose delivery time --</option>
+                  <option value="morning">Morning (8:00–12:00)</option>
+                  <option value="afternoon">Afternoon (12:00–16:00)</option>
+                  <option value="evening">Evening (16:00–20:00)</option>
+                </Field>
+              </div>
+              {/* ------  equipment ------ */}
+              <div>
+                <label>
+                  <Field type="checkbox" name="restrictions" value="vegan" />
+                  Vegan
+                </label>
+                <label>
+                  <Field
+                    type="checkbox"
+                    name="restrictions"
+                    value="gluten-free"
+                  />
+                  Gluten-free
+                </label>
+                <label>
+                  <Field type="checkbox" name="restrictions" value="nut-free" />
+                  Nut-free
+                </label>
+              </div>
+              {/* -----  type------   */}
+              <div>
+                <label>
+                  <Field type="radio" name="delivery" value="pickup" />
+                  Pickup
+                </label>
+                <label>
+                  <Field type="radio" name="delivery" value="courier" />
+                  Courier
+                </label>
+                <label>
+                  <Field type="radio" name="delivery" value="drone" />
+                  Drone delivery
+                </label>
+              </div>
+              <button type="submit">Place order</button>
+            </Form>
+          </Formik>
+        </div>
+      </section>
+    </>
   );
 };
-
+// ////////////////////  ------------    Валідація з Yup
 export default AllFilters;
