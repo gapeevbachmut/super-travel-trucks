f14a6d4ce453222170ef6bf7bec434c59ee28275
Андрій <gapeev1979@gmail.com>
2025-12-07 21:14:36 +0200

    i`m trying

--- components/CamperDetails/Reviews.tsx
+++ components/CamperDetails/Reviews.tsx
@@ -1,19 +1,17 @@
 import { Camper } from "@/types/camper";
+import css from "./Reviews.module.css";
 
 type Props = { camper: Camper };
 
 const Reviews = ({ camper }: Props) => {
   return (
-    <div>
-      <div>
-        <p>{camper.id}</p>
-      </div>
+    <div className={css.reviewWrapper}>
       <div>
         {camper.reviews.map((review, index) => (
           <div key={index}>
-            <h4>{review.reviewer_name}</h4>
+            <p className={css.name}>{review.reviewer_name}</p>
             <p>⭐ {review.reviewer_rating}</p>
-            <p>{review.comment}</p>
+            <p className={css.comment}>{review.comment}</p>
           </div>
         ))}
       </div>
