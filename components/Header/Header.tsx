6e30eed402b8e9bc29ac9cb0ca80597befc37976
Андрій <gapeev1979@gmail.com>
2025-12-11 23:40:13 +0200

    add adaptive design for Home page

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,42 +5,42 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 const Header = () => {
-  // const router = useRouter(); // Отримуємо об'єкт маршрутизатора
-  const currentPath = usePathname(); // Отримуємо поточний шлях сторінки
-
-  // Визначаємо шляхи
+  // для кольору текста сторінки
+  const currentPath = usePathname();
   const homePath = '/';
   const catalogPath = '/catalog';
   return (
     <header className={css.header}>
-      <div className={css.logo}>
-        <Link href="/">
-          <svg className={css.logo} width={136} height={16}>
-            <use href="/mainPage/logo.svg"></use>
-          </svg>
-        </Link>
-      </div>
+      <div className={css.headerLinks}>
+        <div className={css.logo}>
+          <Link href="/" className={css.link}>
+            <svg width={136} height={16}>
+              <use href="/mainPage/logo.svg"></use>
+            </svg>
+          </Link>
+        </div>
 
-      <nav className={css.headerNav}>
-        <ul className={css.navigation}>
-          <li className={css.navigationHome}>
-            <Link
-              href={homePath}
-              className={currentPath === homePath ? css.activeLink : ''}
-            >
-              Home
-            </Link>
-          </li>
-          <li className={css.navigationCatalog}>
-            <Link
-              href={catalogPath}
-              className={currentPath === catalogPath ? css.activeLink : ''}
-            >
-              Catalog
-            </Link>
-          </li>
-        </ul>
-      </nav>
+        <nav className={css.headerNav}>
+          <ul className={css.navigation}>
+            <li className={css.navigationHome}>
+              <Link
+                href={homePath}
+                className={`${css.link} ${currentPath === homePath ? css.activeLink : ''}`}
+              >
+                Home
+              </Link>
+            </li>
+            <li className={css.navigationCatalog}>
+              <Link
+                href={catalogPath}
+                className={`${css.link} ${currentPath === catalogPath ? css.activeLink : ''}`}
+              >
+                Catalog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
